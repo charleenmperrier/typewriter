935b05e37d6abff4844f9ce9b0067c5ec74eb167
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-09 20:21:07 +0000

    using setTimeout for first time

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs \n";
+let timeIncrease = 0;
+
+for (const char of sentence) {
+  timeIncrease += 50
+
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, timeIncrease)
+  
+}
